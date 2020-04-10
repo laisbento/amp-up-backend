ae71ec4bd6ba6623489b82211b3ddeea00e0a3d9
Lais Bento <laismedeiros92@gmail.com>
2020-04-10 19:32:11 -0300

    fix db connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+const connection = knex(PROCESS.env.ENV_URL);
 
 module.exports = connection;
